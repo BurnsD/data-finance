e6d40550e035295c2dfbe8674eb9e912f3fb20fa
BurnsD <Dustinburnsactech@gmail.com>
2022-05-01 16:32:55 -0500

    navbar components list made

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-        <h1>REACT.</h1>
+    <div className='text-white'>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a]'> REACT.</h1>
+        <ul>
+            <li>Home</li>
+            <li>Company</li>
+            <li>Resources</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
     </div>
   )
 }
